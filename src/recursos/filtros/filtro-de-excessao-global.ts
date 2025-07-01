8e80ecb2c138e9077753a361cad348efa0423495
Eduardo <eduardo.rocha2502@gmail.com>
2025-07-01 16:22:47 -0300

    feat: mostrando log de acesso em caso de erro

--- src/recursos/filtros/filtro-de-excessao-global.ts
+++ src/recursos/filtros/filtro-de-excessao-global.ts
@@ -24,6 +24,10 @@ export class FiltroDeExcecaoGlobal implements ExceptionFilter {
     const resposta = contexto.getResponse();
     const requisicao = contexto.getRequest();
 
+    if ('usuario' in requisicao) {
+      this.loggerNativo.error(`Rota acessada pelo usuário ${requisicao.usuario.sub}`);
+    }
+
     const { status, body } =
       exception instanceof HttpException
         ? {
